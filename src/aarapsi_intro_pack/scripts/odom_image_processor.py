55ac54da35ad1fbbb614fb3126268c65a63b2267
Owen Claxton <claxtono@qut.edu.au>
2023-02-08 16:52:17 +1000

    Updates to odom_image_processor.py (now healthy)

--- src/aarapsi_intro_pack/scripts/odom_image_processor.py
+++ src/aarapsi_intro_pack/scripts/odom_image_processor.py
@@ -7,23 +7,29 @@ from cv_bridge import CvBridge
 import cv2 as cv
 import numpy as np
 import rospkg
+from pathlib import Path
+import time
 
 class mrc: # main ROS class
     def __init__(self):
         self.robot_x    = 0
         self.robot_y    = 0
         self.robot_z    = 0
-        self.frame_n    = 0
+        self.frame_n    = 0 # n'th image recorded
 
         rospy.init_node('odom_image_processor', anonymous=True)
-        self.rate_num   = 5 # Hz
-        self.rate_obj   = rospy.Rate(1/self.rate_num)
+        self.rate_num   = 0.2 # Hz
+        self.rate_obj   = rospy.Rate(self.rate_num)
 
         self.odom_sub   = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_callback)
-        self.img0_sub   = rospy.Subscriber("/ros_indigosdk_occam/image0", Image, self.img0_callback)
+        self.img0_sub   = rospy.Subscriber("/ros_indigosdk_occam/image0/compressed", CompressedImage, self.img0_callback)
+        # alternative: self.img0_sub   = rospy.Subscriber("/ros_indigosdk_occam/image0", Image, self.img0_callback)
 
-        self.store_img0 = Image()
+        self.bridge     = CvBridge() # to convert sensor_msgs/Image (or CompressedImage) to cv2
+        #self.store_img0 = self.bridge.compressed_imgmsg_to_cv2(CompressedImage, "bgr8") # populate, empty
+        ## alternative: self.store_img0 = self.bridge.imgmsg_to_cv2(Image, "bgr8")
 
+        # flags to denest main loop:
         self.new_img0   = False
 
     def odom_callback(self, msg):
@@ -38,38 +44,51 @@ class mrc: # main ROS class
     # /ros_indigosdk_occam/image0 (sensor_msgs/Image)
     # Store newest image received
 
-        self.store_img0 = msg
+        self.store_img0 = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        # alternative: self.store_img0 = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         self.new_img0 = True
 
 def odom_image_processor():
+    #!# Variables to Update
+    set_name = "set_1" # Name of folder that will house generated information
+
     nmrc    = mrc() # make new class instance
-    bridge  = CvBridge() # to convert sensor_msgs/Image to cv2
+    path_for_dataset = rospkg.RosPack().get_path('aarapsi_intro_pack') + "/data/" + set_name
 
-    rospack = rospkg.RosPack()
+    # Handle checks for folder construction:
+    rospy.loginfo("Attempting to construct data storage system")
+    try:
+        Path(path_for_dataset).mkdir(parents=False, exist_ok=False) # throw both error states
+    except FileNotFoundError:
+        rospy.logerr("Error: parent directory does not exist. Exitting...")
+        return
+    except FileExistsError:
+        rospy.logwarn("Directory already exists - this will overwrite existing data! Pausing in case of error (5s)...")
+        time.sleep(5)
+        rospy.logwarn("Continuing...")
+    Path(path_for_dataset + '/images/').mkdir(parents=True, exist_ok=True)
+    Path(path_for_dataset + '/odo/').mkdir(parents=True, exist_ok=True)
 
-    path_for_dataset = rospack.get_path('aarapsi_intro_pack') + '/data/set_1'
-
+    # Main loop:
     while not rospy.is_shutdown():
 
-        nmrc.rate.sleep()
+        nmrc.rate_obj.sleep()
 
-        if not nmrc.new_img0:
+        if not nmrc.new_img0: # denest
             continue
-
+        
+        # Store a new image and log new odom
         rospy.loginfo("Logging frame {}".format(str(nmrc.frame_n).zfill(6)))
-        image0 = bridge.imgmsg_to_cv2(nmrc.store_img0, "bgr8")
-        cv.imwrite(path_for_dataset + '/images/frame_id_{}.png'.format(str(nmrc.frame_n).zfill(6)), image0)
+        cv.imwrite(path_for_dataset + '/images/frame_id_{}.png'.format(str(nmrc.frame_n).zfill(6)), nmrc.store_img0)
         np.savetxt(path_for_dataset + '/odo/frame_id_{}.csv'.format(str(nmrc.frame_n).zfill(6)), np.array([nmrc.robot_x, nmrc.robot_y, nmrc.robot_z]),delimiter=',')
         nmrc.frame_n += 1
 
+        # Clear denest flags:
         nmrc.new_img0 = False
-
-        
-
-    rospy.loginfo("Exitting node (roscore condition termination).")
 
 if __name__ == '__main__':
     try:
         odom_image_processor()
+        rospy.loginfo("Exit state reached.")
     except rospy.ROSInterruptException:
         pass
