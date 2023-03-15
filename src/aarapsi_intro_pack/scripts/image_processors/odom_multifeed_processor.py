b4c509a022d7337b25152934b77282fa461b844b
Owen Claxton <claxtono@qut.edu.au>
2023-03-15 11:13:49 +1000

    Corrected error;  y vel odom was actually x vel

--- src/aarapsi_intro_pack/scripts/image_processors/odom_multifeed_processor.py
+++ src/aarapsi_intro_pack/scripts/image_processors/odom_multifeed_processor.py
@@ -33,7 +33,7 @@ class mrc: # main ROS class
 
         self.bridge             = CvBridge() # to convert sensor_msgs/CompressedImage to cv2.
 
-        self.odom_sub           = rospy.Subscriber("/odometry/filtered", Odometry, self.odom_callback)
+        self.odom_sub           = rospy.Subscriber("/odom/filtered", Odometry, self.odom_callback)
         self.img_frwd_sub       = rospy.Subscriber("/ros_indigosdk_occam/image0/compressed", CompressedImage, self.img_frwd_callback, queue_size=1) # centre-view
         self.img_rght_sub       = rospy.Subscriber("/ros_indigosdk_occam/image1/compressed", CompressedImage, self.img_rght_callback, queue_size=1) # right-view
         self.img_left_sub       = rospy.Subscriber("/ros_indigosdk_occam/image4/compressed", CompressedImage, self.img_left_callback, queue_size=1) # left-view
@@ -46,7 +46,7 @@ class mrc: # main ROS class
         self.new_odom           = False
 
     def odom_callback(self, msg):
-    # /odometry/filtered (nav_msgs/Odometry)
+    # /odom/filtered (nav_msgs/Odometry)
     # Store new robot position
 
         self.odom = {'t': msg.header.stamp.to_sec(), \
@@ -57,7 +57,7 @@ class mrc: # main ROS class
                                                    float(msg.pose.pose.orientation.z), \
                                                    float(msg.pose.pose.orientation.w)])[2], \
                      'vx': round(msg.twist.twist.linear.x, 3), \
-                     'vy': round(msg.twist.twist.linear.x, 3), \
+                     'vy': round(msg.twist.twist.linear.y, 3), \
                      'vyaw': round(msg.twist.twist.angular.z, 3)
                     }
         self.new_odom           = True
@@ -104,7 +104,7 @@ def plot_images_two_rows(imgs1, imgs2, figsize_in_inches=(5,5)):
 
 def odom_multiimage_processor():
     #!# Variables to Update
-    set_name                    = "s2_cw_z_230222_o0_e1" # Name of folder that will house generated information
+    set_name                    = "s2_ccw_o0_e0_a0" # Name of folder that will house generated information
     pack_name                   = 'aarapsi_intro_pack' # in case it needs to be adjusted
     nmrc                        = mrc() # make new class instance
     path_for_dataset            = rospkg.RosPack().get_path(pack_name) + "/data/" + set_name
