8bc1c129fe9eb0b73fd4aa524f60b9e68b3a4ad6
Owen Claxton <claxtono@qut.edu.au>
2023-03-24 09:37:43 +1000

    wrapper for netvlad - netvlad class wrapper with functions prep, load, transform, feature_query_extract, feature_reference_extract - netvlad cleanup - fixes to download_models so that it is more pythonic and catches an error with importing - applied respective changes to CM_test_script

--- src/aarapsi_intro_pack/scripts/testing/CM_test_script.py
+++ src/aarapsi_intro_pack/scripts/testing/CM_test_script.py
@@ -3,58 +3,41 @@
 import rospkg
 import numpy as np
 import os
-from PIL import Image
 import time
 import rospy
-import sys
 import torch
-import torchvision.transforms as transforms
+import sys
 from patchnetvlad.models.models_generic import get_pca_encoding
-from aarapsi_intro_pack import Timer
+from aarapsi_intro_pack import VPRImageProcessor
 
 gpu_exists=torch.cuda.is_available()
 rospy.init_node('test')
-#################### Open test image ###################################
-#img = Image.open(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/scripts/testing/frame_id_000000.png')
-#ref_path = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/data/raw_ims/QCR_Lab_3_ref/images/'
+#################### Open test data ###################################
 
-img = Image.open(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/scripts/testing/frame_id_000000.png')
-ref_path = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/data/s1_ccw_o0_e0_a0/forward/'
+dbp = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/data/compressed_sets'
+fn = 's1_ccw_o0_e0_a0'
+dims = (64, 64)
 
+ip = VPRImageProcessor()
+ip.npzLoader(dbp, fn, dims)
+ind = int(np.random.rand() * (len(ip.SET_DICT['times']) - 1))
+ft_raw = ip.SET_DICT['img_feats']['RAW']['forward'][ind]
+img = np.dstack((np.reshape(ft_raw, dims),)*3).astype(np.uint8)
 
 #################### NetVLAD test example ##############################
 # config_path = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/src/aarapsi_intro_pack/Patch_NetVLAD/patchnetvlad/configs/netvlad_extract.ini'
 
-from aarapsi_intro_pack import aarapsi_NetVLAD_feature_extract
+from aarapsi_intro_pack.aarapsi_NetVLAD_feature_extract import NetVLAD_Container
 
-model, config = aarapsi_NetVLAD_feature_extract.load_model(gpu_exists, ngpus=int(gpu_exists))
-
-def prep_query_extract(model, cuda, config):
-# Somehow, running this much code 'accelerates' the feature_query_extract
-# This function when ran first typically takes about 1 second
-    device = torch.device("cuda" if cuda else "cpu")
-    img = Image.fromarray(np.zeros((1,1,3),dtype=np.uint8))
-    the_resize = (int(config['feature_extract']['imageresizeh']), int(config['feature_extract']['imageresizew']))
-    mytransform = aarapsi_NetVLAD_feature_extract.input_transform(the_resize)
-    input_data = mytransform(img)
-    with torch.no_grad():
-        input_data = input_data.unsqueeze(dim=0).to(device)
-        image_encoding = model.encoder(input_data)
-        vlad_global = model.pool(image_encoding)
-        vlad_global_pca = get_pca_encoding(model, vlad_global)
+netvlad_obj = NetVLAD_Container(cuda=gpu_exists, ngpus=int(gpu_exists), logger=lambda x: (x))
 
 t1 = rospy.Time.now().to_sec()
-prep_query_extract(model, gpu_exists, config)
+qry_ftrs = netvlad_obj.feature_query_extract(img)
 t2 = rospy.Time.now().to_sec()
 rospy.loginfo("Duration: %s" % (str(t2-t1)))
-t1 = rospy.Time.now().to_sec()
-qry_ftrs = aarapsi_NetVLAD_feature_extract.feature_query_extract(img, model, gpu_exists, config)
-t2 = rospy.Time.now().to_sec()
-rospy.loginfo("Duration: %s" % (str(t2-t1)))
-#sys.exit()
 
 # t1 = rospy.Time.now().to_sec()
-# ref_ftrs = aarapsi_NetVLAD_feature_extract.feature_ref_extract(ref_path, model, ref_path+'../', bool(num_gpus), config)
+# ref_ftrs = aarapsi_NetVLAD_feature_extract.feature_ref_extract(ref_path)
 # t2 = rospy.Time.now().to_sec()
 # rospy.loginfo("Duration: %s" % (str(t2-t1)))
     
--- src/aarapsi_intro_pack/src/aarapsi_intro_pack/aarapsi_NetVLAD_feature_extract.py
+++ src/aarapsi_intro_pack/src/aarapsi_intro_pack/aarapsi_NetVLAD_feature_extract.py
@@ -32,8 +32,6 @@ Features are saved into a nominated output directory, with one file per image pe
 Code is dynamic and can be configured with essentially *any* number of patch sizes, by editing the config files.
 '''
 
-
-import argparse
 import configparser
 import os
 from os.path import join, exists, isfile
@@ -45,142 +43,137 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import numpy as np
+from PIL import Image
 
 from patchnetvlad.tools.datasets import PlaceDataset
 from patchnetvlad.models.models_generic import get_backend, get_model, get_pca_encoding
 from patchnetvlad.tools import PATCHNETVLAD_ROOT_DIR
+from download_models import download_all_models
 
-def input_transform(resize=(480, 640)):
-    if resize[0] > 0 and resize[1] > 0:
-        return transforms.Compose([
-            transforms.Resize(resize),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
-    else:
-        return transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
+class NetVLAD_Container:
+    def __init__(self, logger=print, cuda=False, ngpus=0, imw=640, imh=480, 
+                 batchsize=5, cachebatchsize=5, num_pcs=4096, 
+                 threads=0, resumepath='./pretrained_models/mapillary_WPCA'):
+        self.cuda           = cuda
+        self.ngpus          = ngpus
+        self.logger         = logger
+        self.imw            = imw
+        self.imh            = imh
+        self.batchsize      = batchsize
+        self.cachebatchsize = cachebatchsize
+        self.num_pcs        = num_pcs
+        self.threads        = threads
+        self.resumepath     = resumepath
+        self.transform      = self.input_transform()
 
-def feature_query_extract(img, model, cuda, config):
-    device = torch.device("cuda" if cuda else "cpu")
-
-    the_resize = (int(config['feature_extract']['imageresizeh']), int(config['feature_extract']['imageresizew']))
-    mytransform = input_transform(the_resize)
-    input_data = mytransform(img)
-
-    with torch.no_grad():
-        input_data = input_data.unsqueeze(dim=0).to(device)
-        image_encoding = model.encoder(input_data)
-        vlad_global = model.pool(image_encoding)
-        vlad_global_pca = get_pca_encoding(model, vlad_global)
-
-    return np.squeeze(vlad_global_pca.detach().cpu().numpy())
-
-def feature_ref_extract(dataset_root_dir, model, output_features_dir, cuda, config):
-    device = torch.device("cuda" if cuda else "cpu")
-    ref_filenames = [filename for filename in sorted(os.listdir(dataset_root_dir))  if os.path.isfile(os.path.join(dataset_root_dir, filename))]
-    # np.savetxt(dataset_root_dir+'../dataset_filenames.txt',ref_filenames, fmt='%s')
-    # dataset_file_path = dataset_root_dir+'../dataset_filenames.txt'
-
-    eval_set = PlaceDataset(None, ref_filenames, dataset_root_dir, None, config['feature_extract'])
-
-    if not exists(output_features_dir):
-        makedirs(output_features_dir)
-
-    # output_local_features_prefix = join(output_features_dir, 'patchfeats')
-    output_global_features_filename = join(output_features_dir, 'NetVLAD_feats.npy')
-
-    pool_size = int(config['global_params']['num_pcs'])
-
-    test_data_loader = DataLoader(dataset=eval_set, num_workers=int(config['global_params']['threads']),
-                                  batch_size=int(config['feature_extract']['cacheBatchSize']),
-                                  shuffle=False, pin_memory=(cuda))
-
-    model.eval()
-    with torch.no_grad():
-        tqdm.write('====> Extracting Features')
-        db_feat = np.empty((len(eval_set), pool_size), dtype=np.float32)
-
-        for iteration, (input_data, indices) in \
-                enumerate(tqdm(test_data_loader, position=1, leave=False, desc='Test Iter'.rjust(15)), 1):
-            indices_np = indices.detach().numpy()
-            input_data = input_data.to(device)
-            image_encoding = model.encoder(input_data)
-            vlad_global = model.pool(image_encoding)
-            vlad_global_pca = get_pca_encoding(model, vlad_global)
-            db_feat[indices_np, :] = vlad_global_pca.detach().cpu().numpy()
-
-    np.save(output_global_features_filename, db_feat)
-
-    return db_feat
+        self.load()
+        self.prep()
 
 
-def load_model(cuda, ngpus=1):
-    # assert os.path.isfile(configfile)
-    config = configparser.ConfigParser()
-    # config.read(configfile)
+    def load(self):
 
-    config['feature_extract'] = {'batchsize' : 5, 'cachebatchsize' : 5, 'imageresizew' : 640, 'imageresizeh': 480}
-    config['global_params'] = {'pooling' : 'netvlad', 'resumepath' : './pretrained_models/mapillary_WPCA', 'threads' : 0, 'num_pcs': 4096, 'ngpu': ngpus}
+        self.config = configparser.ConfigParser()
+        self.config['feature_extract'] = {'batchsize': self.batchsize, 'cachebatchsize': self.cachebatchsize, 
+                                    'imageresizew': self.imw, 'imageresizeh': self.imh}
+        self.config['global_params'] = {'pooling': 'netvlad', 'resumepath': self.resumepath, 
+                                'threads': self.threads, 'num_pcs': self.num_pcs, 'ngpu': self.ngpus}
 
-    # config['feature_extract']['batchsize'] = 5
-    # config['feature_extract']['cachebatchsize'] = 5
-    # config['feature_extract']['imageresizew'] = 640
-    # config['feature_extract']['imageresizeh'] = 480
+        if self.cuda and not torch.cuda.is_available():
+            raise Exception("No GPU found")
+        self.device = torch.device("cuda" if self.cuda else "cpu")
 
-    # config['global_params']['pooling'] = 'netvlad'
-    # config['global_params']['resumepath'] = './pretrained_models/mapillary_WPCA'
-    # config['global_params']['threads'] = 0
-    # config['global_params']['num_pcs'] = 4096
-    # config['global_params']['ngpu'] = ngpus
+        encoder_dim, encoder = get_backend()
 
-
-    if cuda and not torch.cuda.is_available():
-        raise Exception("No GPU found, please run with --nocuda")
-
-    device = torch.device("cuda" if cuda else "cpu")
-
-    encoder_dim, encoder = get_backend()
-
-    # must resume to do extraction
-    if config['global_params']['num_pcs'] != '0':
-        resume_ckpt = config['global_params']['resumePath'] + config['global_params']['num_pcs'] + '.pth.tar'
-    else:
-        resume_ckpt = config['global_params']['resumePath'] + '.pth.tar'
-
-    # backup: try whether resume_ckpt is relative to PATCHNETVLAD_ROOT_DIR
-    if not isfile(resume_ckpt):
-        resume_ckpt = join(PATCHNETVLAD_ROOT_DIR, resume_ckpt)
-        if not isfile(resume_ckpt):
-            from download_models import download_all_models
-            download_all_models(ask_for_permission=True)
-
-    if isfile(resume_ckpt):
-        print("=> Trying to load checkpoint '{}'".format(resume_ckpt))
-        checkpoint = torch.load(resume_ckpt, map_location=lambda storage, loc: storage)
-        if config['global_params']['num_pcs'] != '0':
-            assert checkpoint['state_dict']['WPCA.0.bias'].shape[0] == int(config['global_params']['num_pcs'])
-        config['global_params']['num_clusters'] = str(checkpoint['state_dict']['pool.centroids'].shape[0])
-
-        if config['global_params']['num_pcs'] != '0':
-            use_pca = True
+        # must resume to do extraction
+        if int(self.config['global_params']['num_pcs']) > 0:
+            resume_ckpt = self.config['global_params']['resumePath'] + self.config['global_params']['num_pcs'] + '.pth.tar'
         else:
-            use_pca = False
-        model = get_model(encoder, encoder_dim, config['global_params'], append_pca_layer=use_pca)
-        model.load_state_dict(checkpoint['state_dict'])
-        
-        if int(config['global_params']['nGPU']) > 1 and torch.cuda.device_count() > 1:
-            model.encoder = nn.DataParallel(model.encoder)
-            model.pool = nn.DataParallel(model.pool)
-       
-        model = model.to(device)
-        print("=> Successfully loaded checkpoint '{}'".format(resume_ckpt, ))
-    else:
-        raise FileNotFoundError("=> no checkpoint found at '{}'".format(resume_ckpt))
-    model.eval()
+            resume_ckpt = self.config['global_params']['resumePath'] + '.pth.tar'
 
-    return model, config
+        # backup: try whether resume_ckpt is relative to PATCHNETVLAD_ROOT_DIR
+        if not isfile(resume_ckpt):
+            resume_ckpt = join(PATCHNETVLAD_ROOT_DIR, resume_ckpt)
+            if not isfile(resume_ckpt):
+                download_all_models(ask_for_permission=True)
+
+        if isfile(resume_ckpt):
+            self.logger("=> Trying to load checkpoint '{}'".format(resume_ckpt))
+            checkpoint = torch.load(resume_ckpt, map_location=lambda storage, loc: storage)
+            if bool(self.num_pcs):
+                assert checkpoint['state_dict']['WPCA.0.bias'].shape[0] == int(self.config['global_params']['num_pcs'])
+            self.config['global_params']['num_clusters'] = str(checkpoint['state_dict']['pool.centroids'].shape[0])
+
+            self.model = get_model(encoder, encoder_dim, self.config['global_params'], append_pca_layer=bool(self.num_pcs))
+            self.model.load_state_dict(checkpoint['state_dict'])
+            
+            if int(self.config['global_params']['ngpu']) > 1 and torch.cuda.device_count() > 1:
+                self.model.encoder = nn.DataParallel(self.model.encoder)
+                self.model.pool = nn.DataParallel(self.model.pool)
+        
+            self.model = self.model.to(self.device)
+            self.logger("=> Successfully loaded checkpoint '{}'".format(resume_ckpt, ))
+        else:
+            raise FileNotFoundError("=> no checkpoint found at '{}'".format(resume_ckpt))
+        self.model.eval()
+
+    def prep(self):
+    # Somehow, running this much code 'accelerates' the feature_query_extract
+    # This function when ran first typically takes about 1 second
+        input_data = self.transform(Image.fromarray(np.zeros((1,1,3), dtype=np.uint8)))
+        with torch.no_grad():
+            input_data = input_data.unsqueeze(dim=0).to(self.device)
+            image_encoding = self.model.encoder(input_data)
+            vlad_global = self.model.pool(image_encoding)
+            get_pca_encoding(self.model, vlad_global)
+
+    def input_transform(self):
+        return transforms.Compose([
+            transforms.Resize((self.imh, self.imw)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225]),
+        ])
+
+    def feature_query_extract(self, img):
+        if not isinstance(img, Image.Image):
+            img = Image.fromarray(img)
+        input_data = self.transform(img)
+
+        with torch.no_grad():
+            input_data = input_data.unsqueeze(dim=0).to(self.device)
+            image_encoding = self.model.encoder(input_data)
+            vlad_global = self.model.pool(image_encoding)
+            vlad_global_pca = get_pca_encoding(self.model, vlad_global)
+
+        return np.squeeze(vlad_global_pca.detach().cpu().numpy())
+
+    def feature_ref_extract(self, dataset_root_dir, output_features_dir=None):
+        ref_filenames = [filename for filename in sorted(os.listdir(dataset_root_dir)) if os.path.isfile(os.path.join(dataset_root_dir, filename))]
+
+        eval_set = PlaceDataset(None, ref_filenames, dataset_root_dir, None, self.config['feature_extract'])
+        dataLoader = DataLoader(dataset     = eval_set, 
+                                num_workers = self.config['global_params']['threads'],
+                                batch_size  = self.config['feature_extract']['cacheBatchSize'],
+                                shuffle     = False, 
+                                pin_memory  = self.cuda)
+
+        self.model.eval()
+        with torch.no_grad():
+            tqdm.write('====> Extracting Features')
+            db_feat = np.empty((len(eval_set), self.config['global_params']['num_pcs']), dtype=np.float32)
+
+            for (input_data, indices) in tqdm(dataLoader):
+                indices_np              = indices.detach().numpy()
+                input_data              = input_data.to(self.device)
+                image_encoding          = self.model.encoder(input_data)
+                vlad_global             = self.model.pool(image_encoding)
+                vlad_global_pca         = get_pca_encoding(self.model, vlad_global)
+                db_feat[indices_np, :]  = vlad_global_pca.detach().cpu().numpy()
+
+        if not (output_features_dir is None):
+            if not exists(output_features_dir):
+                makedirs(output_features_dir)
+            output_global_features_filename = join(output_features_dir, 'NetVLAD_feats.npy')
+            np.save(output_global_features_filename, db_feat)
+
+        return db_feat
